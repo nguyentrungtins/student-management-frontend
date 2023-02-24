5bd9f65e4cc0a2546a3f8bafb4009080ea36f85f
nguyentrungtin <nguyentrungtin.tech@gmail.com>
2023-02-24 12:00:46 +0700

    add primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#4267B2",
+      },
+    },
   },
   plugins: [],
 };
