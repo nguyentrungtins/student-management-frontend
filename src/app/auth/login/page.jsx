39ad04c851bdb288d3540ec6102d08fb237597d9
nguyentrungtin <nguyentrungtin.tech@gmail.com>
2023-02-26 19:38:04 +0700

    Fix login UI

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -6,24 +6,20 @@ import { FaUserAlt, FaLock } from "react-icons/fa";
 
 export default function Login() {
   return (
-    <div className="bg-slate-50 w-screen h-screen flex justify-center items-center">
+    <div className="bg-[#F0F1F8] w-screen h-screen flex justify-center items-center">
       <div className="flex justify-center p-4 w-[1100px] h-[700px] rounded-md">
-        <div
-          className="w-1/2 p-3 bg-[#365DC1] flex items-center rounded-l-2xl drop-shadow-xl"
-        >
+        <div className="w-1/2 p-3 bg-primary flex items-center rounded-l-2xl drop-shadow-xl">
           <Image src={Content} />
         </div>
-        <div className="w-1/2 p-3 bg-white drop-shadow-xl rounded-r-2xl">
+        <div className="w-1/2 p-3 bg-white drop-shadow-xl rounded-r-2xl pt-16">
           <div className="p-5 flex justify-center">
-            <Image src={Logo} alt="Logo Images" height={100} />
+            <Image src={Logo} alt="Logo Images" height={60} />
           </div>
-          <h1 className="p-4 flex justify-center text-3xl font-semibold">
-            Hello Again!
+          <h1 className="px-4 pt-10 flex justify-center text-3xl font-semibold">
+            Chào Mừng Bạn
           </h1>
-          <div className="p-5 flex justify-center text-sm text-center font-light">
-            We have built a large, rich academic community strong, eager to use
-            knowledge as a tool to create innovation, Innovative breakthroughs
-            and a better world.
+          <div className="py-5 flex justify-center text-sm text-center font-light w-4/6 m-auto mb-0">
+            Bạn hãy đăng nhập bằng tài khoản của mình ở dưới đây!
           </div>
           <div className="flex flex-col w-4/6 ml-auto mr-auto">
             <div className="flex items-center relative mt-5">
@@ -32,25 +28,26 @@ export default function Login() {
                 placeholder="Enter your username ... "
                 className="rounded-xl pl-12 focus:outline-gray-900 w-full p-2.5 border-solid border-2 border-indigo-100"
               />
-              <FaUserAlt className="absolute left-4" />
+              <FaUserAlt className="absolute left-4 text-gray-500" />
             </div>
 
-            <div className="flex items-center relative mt-5 mb-20">
+            <div className="flex items-center relative mt-5 mb-10">
               <input
                 type="password"
                 placeholder="Enter your password ... "
                 className="rounded-xl pl-12 focus:outline-gray-900 w-full p-2.5 border-solid border-2 border-indigo-100"
               />
-              <FaLock className="absolute left-4" />
+              <FaLock className="absolute left-4 text-gray-500" />
             </div>
           </div>
-          <div className="rounded-xl font-semibold mb-5 p-2 bg-sky-700 text-white w-4/6 ml-auto mr-auto text-center hover:cursor-pointer">
+          <div className="rounded-xl font-semibold mb-10 p-2 bg-primary text-white w-4/6 ml-auto mr-auto text-center hover:cursor-pointer">
             Login
           </div>
           <div className="text-center text-sm font-light">
-            Dont have account yet?
-            <div className="text-indigo-700 font-medium">
-              Contact with us now.
+            Nếu có vấn đề thắc mắc nào
+            <div className="font-medium">
+              Liên hệ với chúng tôi
+              <span className="text-primary font-medium"> tại đây.</span>
             </div>
           </div>
         </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#4267B2",
+        primary: "#017AFF",
       },
     },
   },
