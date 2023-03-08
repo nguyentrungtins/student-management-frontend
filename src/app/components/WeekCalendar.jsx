cfcc4d0b6a1102c27854ac9261daac7dc7ec46d3
nguyentrungtin <nguyentrungtin.tech@gmail.com>
2023-03-08 14:21:55 +0700

    Tin

--- src/app/components/WeekCalendar.jsx
+++ src/app/components/WeekCalendar.jsx
@@ -1,3 +1,82 @@
 export default function WeekCalendar() {
-  return <div>abc</div>;
+  return (
+    <div className="grid grid-cols-[70px_auto] grid-rows-[auto-auto] grid-flow-dense">
+      <div className="bg-white">1</div>
+      <div className="grid grid-cols-7 bg-orange-50 p-5">
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+        <div className="">
+          <p>13</p>
+          <p>Monday</p>
+        </div>
+      </div>
+      <div className="bg-orange-50 p-5">3</div>
+      <div className="grid grid-cols-7 p-5 bg-orange-50">
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+        <div className="grid grid-rows-4">
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+          <span>alo</span>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/dashboard/schedules/page.jsx
+++ src/app/dashboard/schedules/page.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import HeaderNavigation from "@/app/components/HeaderNavigation";
 import Calendar from "@/app/components/Calendar";
+import WeekCalendar from "@/app/components/WeekCalendar";
 const schedules = () => {
   return (
     <div className="ml-64">
@@ -9,7 +10,9 @@ const schedules = () => {
         desc="Đăng ký môn học cho học kỳ I 2022-2203"
       />
       <div className="flex mt-6 bg-blue-50 gap-4 h-screen w-full ">
-        <div className="bg-red-100 w-9/12 h-screen rounded-xl p-4">abc</div>
+        <div className="bg-red-100 w-9/12 h-screen rounded-xl p-4">
+          <WeekCalendar />
+        </div>
         <div className="bg-yellow-50 flex-1 rounded-xl p-4">
           <Calendar />
         </div>
